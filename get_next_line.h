95d45cbe69c7300828384a39e8bda58483ffc966
BrendanVKM <62537693+BrendanVKM@users.noreply.github.com>
2024-06-05 20:56:28 +0200

    test

--- get_next_line.h
+++ get_next_line.h
@@ -5,19 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/28 14:50:24 by bvictoir          #+#    #+#             */
-/*   Updated: 2024/05/28 14:53:03 by bvictoir         ###   ########.fr       */
+/*   Created: 2024/03/23 14:05:49 by bvkm              #+#    #+#             */
+/*   Updated: 2024/06/05 20:38:42 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <stdio.h> 
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 15
+# endif
 
 char	*get_next_line(int fd);
+int		ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2, size_t len);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,9 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/28 14:50:30 by bvictoir          #+#    #+#             */
-/*   Updated: 2024/05/28 14:50:35 by bvictoir         ###   ########.fr       */
+/*   Created: 2024/06/05 19:03:55 by bvictoir          #+#    #+#             */
+/*   Updated: 2024/06/05 20:38:08 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int		ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	
+	if (!s2)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+		while (s1[i])
+			str[i++] = s1[j++];
+	j = 0;
+	while (i < len)
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
